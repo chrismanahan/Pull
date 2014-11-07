d01f2b2f0a00933bab3ecb884e50c8c2fe36e670
Chris Manahan <chrismanahan.dev@gmail.com>
2014-11-06 19:51:23 -0500

    Initial Commit

---
+++ Pull/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pull
+//
+//  Created by Chris Manahan on 11/6/14.
+//  Copyright (c) 2014 Pull LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

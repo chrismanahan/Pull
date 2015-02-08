9fa1d86f15a23da7df5d1765136ffdfcca295143
Chris Manahan <chrismanahan.dev@gmail.com>
2015-02-08 15:42:44 -0500

    finished settings view layout

--- Pull/Classes/View Controllers/PULSettingsViewController.m
+++ Pull/Classes/View Controllers/PULSettingsViewController.m
@@ -14,8 +14,10 @@
 
 @property (strong, nonatomic) IBOutlet UISwitch *notifInviteSwitch;
 @property (strong, nonatomic) IBOutlet UISwitch *notifAcceptSwitch;
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @property (nonatomic, assign) BOOL dirty;
+@property (strong, nonatomic) IBOutlet UILabel *versionLabel;
 
 @end
 
@@ -28,8 +30,35 @@
     PULUserSettings *settings = [PULAccount currentUser].settings;
     [_notifAcceptSwitch setOn:settings.notifyAccept];
     [_notifInviteSwitch setOn:settings.notifyInvite];
+    
 }
 
+
+- (void)viewDidLoad
+{
+    // set version label
+    NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
+    NSString *appVersion = [infoDict objectForKey:@"CFBundleShortVersionString"];
+    NSNumber *buildNumber = [infoDict objectForKey:@"CFBundleVersion"];
+   
+    _versionLabel.text = [NSString stringWithFormat:@"Version %@ (%@)", appVersion, buildNumber];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    CGFloat scrollViewHeight = 0.0f;
+    for (UIView* view in _scrollView.subviews)
+    {
+        CGFloat maxY = CGRectGetMinY(view.frame) + CGRectGetHeight(view.frame);
+        if (maxY > scrollViewHeight)
+        {
+            scrollViewHeight = maxY;
+        }
+    }
+    
+    [_scrollView setContentSize:(CGSizeMake(CGRectGetWidth(_scrollView.frame), scrollViewHeight))];
+}
 
 #pragma mark - actions
 - (IBAction)ibInvite:(id)sender

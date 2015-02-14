784a2c21b80ec6dee93e78f467ac518c85587644
Chris Manahan <chrismanahan@gmail.com>
2015-02-14 15:57:36 -0500

    removed border from user image

--- Pull/Classes/PULUserImageView.m
+++ Pull/Classes/PULUserImageView.m
@@ -10,7 +10,7 @@
 
 @implementation PULUserImageView
 
-IB_DESIGNABLE
+//IB_DESIGNABLE
 
 - (void)setSelected:(BOOL)selected
 {
@@ -21,14 +21,15 @@ IB_DESIGNABLE
 
 - (UIColor*)borderColor
 {
-    if (!_borderColor)
-    {
-        return [UIColor whiteColor];
-    }
-    else
-    {
-        return _borderColor;
-    }
+    return [UIColor whiteColor];
+//    if (!_borderColor)
+//    {
+//        return [UIColor whiteColor];
+//    }
+//    else
+//    {
+//        return _borderColor;
+//    }
 }
 
 - (void)layoutSubviews
@@ -38,7 +39,7 @@ IB_DESIGNABLE
     self.imageView.frame = CGRectMake(0, 0, CGRectGetWidth(self.frame), CGRectGetHeight(self.frame));
     self.imageView.center = CGPointMake(CGRectGetWidth(self.frame) / 2, CGRectGetHeight(self.frame) / 2);
     
-    NSInteger offset = 12;
+    NSInteger offset = 0;//12;
     CAShapeLayer *circle = [CAShapeLayer layer];
     // Make a circular shape
     UIBezierPath *circularPath = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(offset / 2, offset / 2, self.imageView.frame.size.width - offset, self.imageView.frame.size.height - offset) cornerRadius:MAX(self.imageView.frame.size.width, self.imageView.frame.size.height)];
@@ -62,17 +63,17 @@ IB_DESIGNABLE
     rect = CGRectInset(rect, 2, 2);
     CGContextRef ref = UIGraphicsGetCurrentContext();
     
-    CGContextSetShadow(ref, CGSizeMake(1, 1), 2);
+    CGContextSetShadow(ref, CGSizeMake(0, 2), 2);
     
-    CGColorRef color;
-    if (!_selected)
-    {
-        color = self.borderColor.CGColor;// [UIColor whiteColor].CGColor;
-    }
-    else
-    {
-        color = [UIColor colorWithRed:0.537 green:0.184 blue:1.000 alpha:1.000].CGColor;
-    }
+    CGColorRef color = [UIColor whiteColor].CGColor;
+//    if (!_selected)
+//    {
+//        color = self.borderColor.CGColor;// [UIColor whiteColor].CGColor;
+//    }
+//    else
+//    {
+//        color = [UIColor colorWithRed:0.537 green:0.184 blue:1.000 alpha:1.000].CGColor;
+//    }
     
     CGContextSetFillColorWithColor(ref, color);
     

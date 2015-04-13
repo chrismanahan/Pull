5a61893aef9bdd9369a505bd2378efacf69c7139
Chris Manahan <chrismanahan.dev@gmail.com>
2015-04-13 17:02:45 -0400

    Put login into unit test category to use in all tests. Created unit test for blocking and unblocking

---
+++ PullTests/Categories/XCTestCase+Auth.h
@@ -0,0 +1,13 @@
+//
+//  XCTestCase+Auth.h
+//  Pull
+//
+//  Created by Development on 4/11/15.
+//  Copyright (c) 2015 Pull LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XCTestCase (Auth)
+
+@end
---
+++ PullTests/Categories/XCTestCase+Auth.m
@@ -0,0 +1,13 @@
+//
+//  XCTestCase+Auth.m
+//  Pull
+//
+//  Created by Development on 4/11/15.
+//  Copyright (c) 2015 Pull LLC. All rights reserved.
+//
+
+#import "XCTestCase+Auth.h"
+
+@implementation XCTestCase (Auth)
+
+@end

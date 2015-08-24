13e8fdc00517199599aaafc5db5d6262c136928d
Chris Manahan <chris@getpulled.com>
2015-08-23 23:56:55 -0400

    Closes T176 - Fixed lag between main view and menu

--- Pull/Classes/Segues/PULSlideSegue.m
+++ Pull/Classes/Segues/PULSlideSegue.m
@@ -46,9 +46,13 @@
                          
                          dispatch_async(dispatch_get_main_queue(), ^{
                              [sourceViewController presentViewController:destinationViewController animated:NO completion:NULL]; // present VC
+                             
+                             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                                   [destView removeFromSuperview]; // remove from temp super view
+                             });
+
                          });
                          
-//                         [destView removeFromSuperview]; // remove from temp super view
 //                         destView.frame = origFrame;
                      }];
 }

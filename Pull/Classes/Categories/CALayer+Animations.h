bed11a48c5a7544aa9e131ab3aac0ce83b335ef0
Chris Manahan <chris@getpulled.com>
2015-08-24 20:31:07 -0400

    Reworked calayer animation category a bit

--- Pull/Classes/Categories/CALayer+Animations.h
+++ Pull/Classes/Categories/CALayer+Animations.h
@@ -11,5 +11,6 @@
 @interface CALayer (Animations)
 
 - (void)addPopAnimation;
+- (void)addPopAnimationCompletion:(void(^)())completion;
 
 @end
--- Pull/Classes/Categories/CALayer+Animations.m
+++ Pull/Classes/Categories/CALayer+Animations.m
@@ -12,22 +12,38 @@
 
 - (void)addPopAnimation
 {
+    [self addPopAnimationCompletion:nil];
+}
+
+- (void)addPopAnimationCompletion:(void(^)())completion;
+{
+    [self _addPopFrom:1.0 to:1.2 completion:completion];
+}
+
+- (void)_addPopFrom:(CGFloat)from to:(CGFloat)to completion:(void(^)())completion;
+{
     CAAnimation *existing = [self animationForKey:@"pop"];
     
     if (!existing)
     {
         [CATransaction begin]; {
             [CATransaction setCompletionBlock:^{
+
                 [self removeAnimationForKey:@"pop"];
+                
+                if (completion)
+                {
+                    completion();
+                }
             }];
             CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
             anim.duration = 0.1;
-            anim.fromValue = @(1.0);
-            anim.toValue = @(1.2);
+            anim.fromValue = @(from);
+            anim.toValue = @(to);
             anim.autoreverses = YES;
+            
             [self addAnimation:anim forKey:@"pop"];
         } [CATransaction commit];
-        
     }
 }
 
